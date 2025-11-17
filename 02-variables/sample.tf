a46f7483b09212a561a83ded7bc982e0e1e3819d
Revanthsatyam <satyamrevanth@gmail.com>
2025-11-17 17:58:54 +0530

    terraform

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -1,16 +1,67 @@
-variable "x1"{
+# x1=abc # shell
+# x1: abc # ansible
 
-  default= "abc"
+variable "x1" {
+  default = "abc"
 }
 
-output "x1"{
-  value=var.x1
+output "x1" {
+  # variable var.x1 for the direct variable usage
+  value = var.x1
 }
 
-variable "x2"{
-  default="123"
+output "x11" {
+  # variable var.x1 is with combination of a string then ${var.x1}
+  value = "Value of x1 is ${var.x1}"
 }
 
-output "x2"{
-  value="value of x2 is: ${var.x2}"
+# Data types
+# String , Need to be given in double quotes
+# Number, No need of quote
+# Boolean, No need of quote
+
+variable "string1" {
+  default = "abc"
+}
+
+variable "number1" {
+  default = 100
+}
+
+variable "number2" {
+  default = 100.1
+}
+
+# true or false is supported.
+variable "boolean1" {
+  default = true
+}
+
+# Variable Types
+# Plain
+# List
+# Map / Dict
+
+variable "plain1" {
+  default = "abc"
+}
+
+# list index starts from 0
+variable "list1" {
+  default = ["abc", 123, true]
+}
+
+variable "map1" {
+  default = {
+    name = "devops"
+    skill = "engineer"
+  }
+}
+
+output "list1_value1" {
+  value = var.list1[0]
+}
+
+output "map1_name" {
+  value = var.map1["name"]
 }
