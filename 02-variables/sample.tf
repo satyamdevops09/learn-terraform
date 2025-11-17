689f6ad40d74dc25f596067de696c87c416b7b39
Revanthsatyam <satyamrevanth@gmail.com>
2025-11-17 18:11:40 +0530

    terraform

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -68,3 +68,10 @@ output "map1_name" {
 
 # terraform.tfvars  -> This file will be picked automatically
 variable "x3" {}
+
+# dev.tfvars, prod.tfvars -> this needs to be exclusively passed with -var-file from cli
+variable "x3_env" {}
+
+output "x3_env" {
+  value = var.x3_env
+}
